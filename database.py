572d31249aff3716eb9e17455550440c16b9af08
k-wakayama-gh <kwakayama01@gmail.com>
2024-01-14 02:01:14 +0900

    revert

--- database.py
+++ database.py
@@ -4,8 +4,14 @@
 from sqlmodel import SQLModel, create_engine, Session
 import os
 
+env = "AZURE"
+mount = "/azstorage1"
 
-db_file = 'sqlite:///azstorage1/database.sqlite'
+if env in os.environ:
+    db_file = f'sqlite:///{mount}/database.sqlite'
+else:
+    db_file = 'sqlite:///database.sqlite'
+
 
 # database settings
 engine = create_engine(db_file, echo=False, connect_args={'check_same_thread': False})
