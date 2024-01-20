1d15453daa01f58eb67af8575757efa71fdeddc8
k-wakayama-gh <kwakayama01@gmail.com>
2024-01-21 01:29:33 +0900

    sql server

--- database.py
+++ database.py
@@ -5,21 +5,13 @@ from sqlmodel import SQLModel, create_engine, Session
 import os
 import shutil
 
-db_file = 'database.sqlite'
 
 # env1 = "IN_DOCKER_CONTAINER"
 env = "WEBSITES_ENABLE_APP_SERVICE_STORAGE"
 
-if env in os.environ:
-    db_dir = "/home/db_dir"
-    db_path = f"{db_dir}/{db_file}"
-else:
-    db_dir = ""
-    db_path = f"{db_file}"
 
-
-remote_db = f"{db_dir}/{db_file}"
-local_db = f"/{db_file}"
+remote_db = "/home/mount/database.sqlite"
+local_db = "database.sqlite"
 
 
 def load_db():
@@ -35,10 +27,15 @@ def save_db():
         pass
 
 
-sqlite_connection = f'sqlite:///{db_path}'
+
+sql_connection = f'sqlite:///{local_db}'
+
+if env in os.environ:
+    sql_connection = 'Server=tcp:m9-m9-m9.database.windows.net,1433;Initial Catalog=m9-m9-m9;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication="Active Directory Default";'
+
 
 # database settings
-engine = create_engine(sqlite_connection, echo=False, connect_args={'check_same_thread': False})
+engine = create_engine(sql_connection, echo=False, connect_args={'check_same_thread': False})
 
 # def : create the database
 def create_database():
