58357a8cb3fb61ed7aff95992f436daa91928ed4
k-wakayama-gh <kwakayama01@gmail.com>
2024-02-11 19:40:46 +0900

    changed remote db path

--- main.py
+++ main.py
@@ -27,7 +27,10 @@ app.mount('/static', StaticFiles(directory='static'), name='static')
 # create database on startup
 @app.on_event("startup")
 def on_startup():
-    load_db()
+    try:
+        load_db()
+    except:
+        print("error: failed to load db")
     create_database()
 
 @app.on_event("shutdown")
