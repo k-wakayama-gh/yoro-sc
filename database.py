5107f6c27d1f87b33d2e457c846b50bbb060e563
k-wakayama-gh <kwakayama01@gmail.com>
2024-02-12 20:36:20 +0900

    small fixes

--- database.py
+++ database.py
@@ -13,51 +13,37 @@ db_file = 'database.sqlite'
 
 remote_db_dir = "/home/site/wwwroot/db_dir/"
 
-# if env in os.environ:
-#     if not os.path.exists(remote_db_dir):
-#         os.makedirs(remote_db_dir)
-#         print(f"Directory {remote_db_dir} has been created.")
-#     else:
-#         print(f"Directory {remote_db_dir} already exists.")
-
-
+local_db = db_file
 remote_db = f"{remote_db_dir}{db_file}"
 
 
-# if env in os.environ:
-#     local_db = f"/home/site/wwwroot/db_dir/{db_file}"
-# else:
-#     local_db = db_file
-
-local_db = db_file
-
-
-# copy data
-def load_db():
+def make_remote_db_dir():
     if env in os.environ:
         if not os.path.exists(remote_db_dir):
             os.makedirs(remote_db_dir)
             print(f"Directory {remote_db_dir} has been created.")
         else:
             print(f"Directory {remote_db_dir} already exists.")
-        if os.path.exists(remote_db_dir):
-            shutil.copy(remote_db, local_db)
-            print(f"Copyed {remote_db} to {local_db}")
-        else:
-            print("Remote directory not found")
+
+
+# load and save db
+def load_db():
+    make_remote_db_dir()
+    if os.path.exists(remote_db_dir):
+        shutil.copy(remote_db, local_db)
+        print(f"Copyed {remote_db} to {local_db}")
+    else:
+        print("Remote directory not found")
+
 
 def save_db():
-    if env in os.environ:
-        if not os.path.exists(remote_db_dir):
-            os.makedirs(remote_db_dir)
-            print(f"Directory {remote_db_dir} has been created.")
-        else:
-            print(f"Directory {remote_db_dir} already exists.")
-        if os.path.exists(remote_db_dir):
-            shutil.copy(local_db, remote_db)
-            print(f"Copyed {local_db} to {remote_db}")
-        else:
-            print("Remote directory not found")
+    make_remote_db_dir()
+    if os.path.exists(remote_db_dir):
+        shutil.copy(local_db, remote_db)
+        print(f"Copyed {local_db} to {remote_db}")
+    else:
+        print("Remote directory not found")
+
 
 
 db_connection = f'sqlite:///{local_db}'
@@ -66,9 +52,11 @@ db_connection = f'sqlite:///{local_db}'
 # database settings
 engine = create_engine(db_connection, echo=False, connect_args={'check_same_thread': False})
 
+
 # def : create the database
 def create_database():
     SQLModel.metadata.create_all(engine)
+
 
 def get_session():
     with Session(engine) as session:
